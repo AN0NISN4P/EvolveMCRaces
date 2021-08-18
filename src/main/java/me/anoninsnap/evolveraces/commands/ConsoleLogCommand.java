dfd64edae978cd2734090da80f294fbcd62bab28
Young <andreasyoungandersen@gmail.com>
2021-08-18 19:52:44 +0200

    Optimisations to command can now enable and disable using multiple words yes/no, true/false, on/off, enable/disable

--- src/main/java/me/anoninsnap/evolveraces/commands/ConsoleLogCommand.java
+++ src/main/java/me/anoninsnap/evolveraces/commands/ConsoleLogCommand.java
@@ -1,30 +1,32 @@
 package me.anoninsnap.evolveraces.commands;
 
 import me.anoninsnap.evolveraces.development.ConsoleLogger;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 public class ConsoleLogCommand implements CommandExecutor {
-	Set<String> enable = Collections.emptySet();
-	Set<String> disable = Collections.emptySet();
+	Set<String> enable = new HashSet<>();
+	Set<String> disable = new HashSet<>();
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (enable.isEmpty() || disable.isEmpty()){
+		if (enable.size() == 0 || disable.size() == 0) {
 			Collections.addAll(enable, "yes", "true", "on", "enable");
 			Collections.addAll(disable, "no", "false", "off", "disable");
 		}
 
 		if (sender.isOp() && args.length != 0) {
 			String arg = args[0];
-			if (arg.equalsIgnoreCase("true")) {
+			if (enable.contains(arg)) {
 				ConsoleLogger.logToConsole = true;
-			} else if (arg.equalsIgnoreCase("false")) {
+			} else if (disable.contains(arg)) {
 				ConsoleLogger.logToConsole = false;
 			} else {
 				sender.sendMessage(ChatColor.RED + "/consoleLog <true/false>");
