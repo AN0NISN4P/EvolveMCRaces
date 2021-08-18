8b6c7c116f3b48c2c3f6829bf4b487164304dd8f
Young <andreasyoungandersen@gmail.com>
2021-08-18 19:53:22 +0200

    Configs loading

--- src/main/java/me/anoninsnap/evolveraces/EvolveRaces.java
+++ src/main/java/me/anoninsnap/evolveraces/EvolveRaces.java
@@ -5,6 +5,7 @@ import me.anoninsnap.evolveraces.commands.GetRaceCommand;
 import me.anoninsnap.evolveraces.commands.SetRaceCommand;
 import me.anoninsnap.evolveraces.development.ConsoleLogger;
 import me.anoninsnap.evolveraces.eventlisteners.PlayerHitListener;
+import me.anoninsnap.evolveraces.timedevents.PlayerTimedEffects;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EvolveRaces extends JavaPlugin {
@@ -14,15 +15,29 @@ public final class EvolveRaces extends JavaPlugin {
 		// Plugin startup logic
 		ConsoleLogger.logToConsole = true;
 
-
+		// Load races
 		PlayerRaceLists.init();
+
+		// Default Config
+		defaultConfigs();
+
+		// Set Commands
 		getCommand("setrace").setExecutor(new SetRaceCommand());
 		getCommand("getrace").setExecutor(new GetRaceCommand());
 		getCommand("consoleLog").setExecutor(new ConsoleLogCommand());
 
+		// Start Recurring Events
+		Runnable timedEvents = new PlayerTimedEffects(this);
+		new PlayerHitListener(this);
 
-		getServer().getPluginManager().registerEvents(new PlayerHitListener(), this);
 
+		// Schedule Recurring Events
+		getServer().getScheduler().scheduleSyncRepeatingTask(this, timedEvents, 10, 10); // TODO: Config File
+	}
+
+	private void defaultConfigs() {
+		getConfig().options().copyDefaults();
+		saveDefaultConfig();
 	}
 
 	@Override
